3d049ae942a0d90e8c617ece312832c7245983c3
emerykurt <emerylumsden@gmail.com>
2020-07-24 01:13:06 -0400

    user route started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,32 @@
 class UsersController < ApplicationController
 
     def create
+        @user = User.create(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def new
+        @user = User.new
     end
 
     def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @shoe.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        user = User.find(params[:id]) 
+        user.destroy
+        redirect_to eateries_path
     end
 
     def show
@@ -16,5 +36,11 @@ class UsersController < ApplicationController
     end
 
     def logout
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :email, :password_digest)
     end
 end
